718da801936c0ba18707ca243d2cd632f958bb8b
Eulenda <165171519+ThembaniJubilant@users.noreply.github.com>
2024-06-14 15:35:35 +0200

    Removed commented code

--- expensify-integration/expensify-integration/src/main/java/com/kalakec/expensify_integration/service/ExpensifyService.java
+++ expensify-integration/expensify-integration/src/main/java/com/kalakec/expensify_integration/service/ExpensifyService.java
@@ -9,10 +9,11 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
-
+@Service
 public class ExpensifyService {
 
     @Autowired
